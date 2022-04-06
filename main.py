dd983f9dfac8c2c18bf646bf448fbabbcb93fbdd
RadOctocode <RadOctocode>
2022-04-05 21:34:08 -0400

    api call works and returns dict of colors and number of pips

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 # main.py
+import re
 import sys
+from defer import return_value
 import requests
+from time import sleep
+
+VALID_COLOR = {"W":"Plains","U":"Island","B":"Swamp","R":"Mountain","G":"Forest","C":"Wastes"}
 
 def find_colored_pips(card_name):
     """
         returns a dict of colored pips
     """
-    response = requests.get("https://api.scryfall.com/cards/named?exact=Austere Command")
-    mana_cost = response.json()['mana_cost']
-    return mana_cost
+    response = requests.get("https://api.scryfall.com/cards/named?exact="+card_name)
+   
+    mana_cost = response.json()['mana_cost'].strip()
+    mana_cost = mana_cost.replace("{"," ")
+    mana_cost = mana_cost.replace("}"," ")
+    mana_cost = mana_cost.replace("/"," ")
+    mana_cost = mana_cost.replace("P"," ")
+    mana_cost_list = mana_cost.split(" ")
+
+    return_dict = dict()
+    return_total = 0
+
+    for value in mana_cost_list:
+        if value in VALID_COLOR:
+            if value in return_dict:
+                return_dict[value] += 1
+            else:
+                return_dict[value] = 1
+            return_total += 1
+
+    return return_total, return_dict
 
 def remove_sideboards(line_list):
     """
@@ -32,10 +55,17 @@ def find_total_cmc(decklist):
     """
     with open(decklist,'r') as file:
         deck_lines = remove_sideboards(file.readlines())
+        total_deck_pips = 0
+        total_deck_dict = {"W":0, "U":0, "B":0, "R":0, "G":0, "C":0}
+        
         for line in deck_lines:
                 card_name = line.split(" ",1)[1]
-        print(find_colored_pips("test"))
-        print("finished")
+                card_total_pips, card_pip_dict = find_colored_pips(card_name)
+                for key in card_pip_dict:  
+                    total_deck_dict[key] += card_pip_dict[key]
+                    total_deck_pips += card_total_pips
+                sleep(0.1)
+        
 
         return decklist
     
